55cf0005ab183c9cdecf80902d8265f9cdbd9dae
Sam <jus7coding@gmail.com>
2026-06-26 00:59:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 const channels = [
      {
     name: "TSN",
-    url: "https://xyzstreams.shop/wc-7-embed.html"
+    url: "https://xyzstreams.st/wc-7-embed.html"
   },
      {
     name: "FS1",
-    url: "https://xyzstreams.shop/wc-2-embed.html"
+    url: "https://xyzstreams.st/wc-2-embed.html"
   },
      {
     name: "FOX",
-    url: "https://xyzstreams.shop/wc-1-embed.html"
+    url: "https://xyzstreams.st/wc-1-embed.html"
   },
 {
     name: "STV",
-    url: "https://xyzstreams.shop/wc-3-embed.html"
+    url: "https://xyzstreams.st/wc-3-embed.html"
   },
      {
     name: "ITV 4",
-    url: "https://xyzstreams.shop/wc-15-embed.html"
+    url: "https://xyzstreams.st/wc-15-embed.html"
   },
   {
     name: "BBC",
-    url: "https://xyzstreams.shop/wc-4-embed.html"
+    url: "https://xyzstreams.st/wc-4-embed.html"
   },
 {
     name: "beIN MAX",
-    url: "https://xyzstreams.shop/wc-11-embed.html"
+    url: "https://xyzstreams.st/wc-11-embed.html"
   },
 {
     name: "Telemundo",
-    url: "https://xyzstreams.shop/wc-6-embed.html"
+    url: "https://xyzstreams.st/wc-6-embed.html"
   },
      {
     name: "UNIVERSO",
-    url: "https://xyzstreams.shop/wc-19-embed.html"
+    url: "https://xyzstreams.st/wc-19-embed.html"
   },
     {
     name: "TSN 4K",
-    url: "https://xyzstreams.shop/wc-8-embed.html"
+    url: "https://xyzstreams.st/wc-8-embed.html"
   },
      {
     name: "FS1 4K",
-    url: "https://xyzstreams.shop/wc-fs14k-embed.html"
+    url: "https://xyzstreams.st/wc-fs14k-embed.html"
   },
   {
     name: "FOX 4K",
-    url: "https://xyzstreams.shop/wc-5-embed.html"
+    url: "https://xyzstreams.st/wc-5-embed.html"
   },
     {
     name: "BEIN MAX 4K",
-    url: "https://xyzstreams.shop/wc-10-embed.html"
+    url: "https://xyzstreams.st/wc-10-embed.html"
   },
 {
     name: "Fussball 4K",
-    url: "https://xyzstreams.shop/wc-14-embed.html"
+    url: "https://xyzstreams.st/wc-14-embed.html"
   },
     {
     name: "Telemundo 4K",
-    url: "https://xyzstreams.shop/wc-12-embed.html"
+    url: "https://xyzstreams.st/wc-12-embed.html"
   },
 ];
 
