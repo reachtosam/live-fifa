6d7d2cf458c06c278e57432f5d7e840c734f5779
Sam <jus7coding@gmail.com>
2026-07-07 01:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const channels = [
-     {
+  {
     name: "TSN",
     url: "https://vileembeds.pages.dev/embed/tsn1-ca"
   },
-  //    {
+  // {
   //   name: "FS1",
   //   url: "https://xyzstreams.st/wc-2-embed.html"
   // },
-     {
+  {
     name: "FOX",
     url: "https://vileembeds.pages.dev/embed/fox-usa"
   },
-// {
-//     name: "STV",
-//     url: "https://xyzstreams.st/wc-3-embed.html"
-//   },
-       {
+  // {
+  //   name: "STV",
+  //   url: "https://xyzstreams.st/wc-3-embed.html"
+  // },
+  {
     name: "DAZN",
     url: "https://vileembeds.pages.dev/embed/daznmundial-es"
   },
-     {
+  {
     name: "DSports",
     url: "https://vileembeds.pages.dev/embed/dsports-ar"
   },
-      {
+  {
     name: "CazeTV",
     url: "https://vileembeds.pages.dev/embed/cazetv-br"
   },
-     {
+  {
     name: "BBC",
     url: "https://vileembeds.pages.dev/embed/bbcone-uk"
   },
@@ -35,27 +35,27 @@ const channels = [
   //   name: "BBC",
   //   url: "https://xyzstreams.st/wc-4-embed.html"
   // },
-// {
-//     name: "beIN Sports 1",
-//     url: "https://xyzstreams-6h9.pages.dev/embed.html?id=bein12fr-xyz"
-//   },
-     {
+  // {
+  //   name: "beIN Sports 1",
+  //   url: "https://xyzstreams-6h9.pages.dev/embed.html?id=bein12fr-xyz"
+  // },
+  {
     name: "beIN Sports MAX",
     url: "https://vileembeds.pages.dev/embed/beinsportsmax-sa"
   },
-{
+  {
     name: "Telemundo",
     url: "https://vileembeds.pages.dev/embed/telemund0-usa"
   },
-  //    {
+  // {
   //   name: "UNIVERSO",
   //   url: "https://xyzstreams.st/wc-19-embed.html"
   // },
-    {
+  {
     name: "TSN 4K",
     url: "https://vileembeds.pages.dev/embed/tsn-4k"
   },
-     {
+  {
     name: "BBC 4K",
     url: "https://vileembeds.pages.dev/embed/bbc-4k-2"
   },
@@ -63,19 +63,19 @@ const channels = [
     name: "FOX 4K",
     url: "https://vileembeds.pages.dev/embed/fox4k-usa"
   },
-    {
+  {
     name: "BEIN MAX 4K",
     url: "https://vileembeds.pages.dev/embed/beinsportsuhd-sa"
   },
-{
+  {
     name: "FUSBALL.TV1 4K",
     url: "https://vileembeds.pages.dev/embed/fussballtv1uhd-de"
   },
-     {
+  {
     name: "FUSBALL.TV1 4K NC",
     url: "https://vileembeds.pages.dev/embed/fussballtvuhd-de"
   },
-  //   {
+  // {
   //   name: "Telemundo 4K",
   //   url: "https://xyzstreams.st/wc-12-embed.html"
   // },
@@ -84,7 +84,6 @@ const channels = [
 const channelList = document.getElementById("channelList");
 const player = document.getElementById("player");
 const channelTitle = document.getElementById("channelTitle");
-const search = document.getElementById("search");
 
 const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
@@ -92,12 +91,10 @@ const overlay = document.getElementById("overlay");
 
 let activeIndex = 0;
 
-function renderChannels(filter = "") {
+function renderChannels() {
   channelList.innerHTML = "";
 
   channels.forEach((channel, index) => {
-    if (!channel.name.toLowerCase().includes(filter.toLowerCase())) return;
-
     const div = document.createElement("div");
     div.className = "channel" + (index === activeIndex ? " active" : "");
     div.textContent = channel.name;
@@ -119,14 +116,10 @@ function loadChannel(index) {
   activeIndex = index;
   player.src = channels[index].url;
   channelTitle.textContent = channels[index].name;
-  renderChannels(search.value);
 
+  renderChannels();
   closeMobileMenu();
 }
-
-search.addEventListener("input", () => {
-  renderChannels(search.value);
-});
 
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
